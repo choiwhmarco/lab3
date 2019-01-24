f5780a5467474b09a1b40c32544c514b7f0e5f07
choiwhmarco <whchoi@ucsd.edu>
2019-01-24 10:33:39 -0800

    update node.js files

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+	var nameToShow = req.params.userName;
+	console.log('name ' + nameToShow);
+	if (nameToShow == null){
+		nameToShow = 'World';
+	}
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
